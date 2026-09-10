96dbb7f27f36a6604b76390f2c86d1eaef8b9c44
Drase Ipatov <steamprimecs123@gmail.com>
2026-09-10 19:39:34 +0500

    Task6

---
+++ Tyuiu.TaranDA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TaranDA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.TaranDA.Sprint0.Task6.V0
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массивов = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(numsArray));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
