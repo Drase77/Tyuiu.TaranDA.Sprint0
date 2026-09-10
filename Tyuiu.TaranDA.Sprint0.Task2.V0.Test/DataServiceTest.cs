f778f40f083e2aab37c7462115c1f9116a08c1b6
Drase Ipatov <steamprimecs123@gmail.com>
2026-09-10 18:02:25 +0500

    Task1

---
+++ Tyuiu.TaranDA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TaranDA.Sprint0.Task2.V0.Lib;
+
+
+namespace Tyuiu.TaranDA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Данил";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Данил", res);
+        }
+    }
+}
---
+++ Tyuiu.TaranDA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.TaranDA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.TaranDA.Sprint0.Task2.V0
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Данил"));
+            Console.ReadKey();
+        }
+    }
+}
